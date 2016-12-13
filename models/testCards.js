ddfb4c2e5f58340393f27f46f382bcb6b18d522f
Jeanine <jeanineh@andrew.cmu.edu>
2016-12-13 13:45:27 -0500

    removed models and views that weren't being used

--- models/testCards.js
+++
@@ -1,8 +0,0 @@
-/* A test file for the deck model */
-
-var deck = require("./deck.js");
-cards = deck.allCards();
-console.log(cards.length);
-card1 = deck.drawFromDeck();
-console.log(card1);
-console.log(card1.word);
